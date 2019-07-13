1a79308bd7dfe67a35af035988735345e8868b08
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-07-13 18:29:37 +0200

    Notify alive works now

--- deadpage-server/src/Handler/NotifyAlive.hs
+++ deadpage-server/src/Handler/NotifyAlive.hs
@@ -1,10 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Handler.NotifyAlive where
 
 import Import
 
 import Network.Consul as Consul
 
+data NotifyAlive =
+  NotifyAlive
+    { notifyAliveTtl :: Maybe Text
+    }
+
+notifyAliveForm :: FormInput Handler NotifyAlive
+notifyAliveForm = NotifyAlive <$> iopt textField "ttl"
+
 postNotifyAliveR :: CheckName -> Handler ()
 postNotifyAliveR cn = do
   cc <- getsYesod appConsulClient
-  passHealthCheck cc cn Nothing
+  NotifyAlive {..} <- runInputPost notifyAliveForm
+  let rs =
+        RegisterService
+          { rsId = Nothing
+          , rsName = cn
+          , rsTags = []
+          , rsPort = Nothing
+          , rsCheck = Just $ Ttl $ fromMaybe "10s" notifyAliveTtl
+          }
+  b <- registerService cc rs Nothing
+  unless b $ error "registerService returned False"
+  passHealthCheck cc ("service:" <> cn) Nothing
